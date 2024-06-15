646e5d16c2ab085483a7b6cdade870fb97f2f0cb
iQuick <root@iquick.dev>
2024-06-15 02:23:26 +0200

    i hate console.logs ffs

--- api/auth/updatePassword.js
+++ api/auth/updatePassword.js
@@ -5,7 +5,6 @@ module.exports = async function (fastify, options) {
 	fastify.post('/password', async (req, res) => {
 		const user = await authenticate(req, res)
 		const { password } = req.body
-		console.log(user)
 		if (!password || password == '') {
 			res.send({ error: 'Invalid password' }).code(403)
 		}
