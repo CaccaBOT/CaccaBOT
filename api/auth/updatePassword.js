254274af5646bc23e5c6113ca21514965ce2ca04
iQuick <iquickpublic@gmail.com>
2024-06-17 22:43:56 +0200

    logger + password requirement

--- api/auth/updatePassword.js
+++ api/auth/updatePassword.js
@@ -5,7 +5,7 @@ module.exports = async function (fastify, options) {
 	fastify.post('/password', async (req, res) => {
 		const user = await authenticate(req, res)
 		const { password } = req.body
-		if (!password || password == '') {
+		if (!password || password.length < 3) {
 			res.code(403).send({ error: 'Invalid password' })
 		}
 		updatePassword(user.id, password)
