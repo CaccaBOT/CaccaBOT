26ebeb5d6f13ad979afebb9375a39067a4461e4e
iQuick <root@iquick.dev>
2024-08-19 00:35:43 +0200

    add version endpoint

---
+++ api/internals/getVersion.js
@@ -0,0 +1,7 @@
+const config = require('../../config.json')
+
+module.exports = async function (fastify, options) {
+	fastify.get('/version', async (req, res) => {
+		res.code(200).send({version: config.version})
+	})
+}
