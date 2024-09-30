93684ebb39f085bfb1958555897c31d7e32c867f
iQuick <iquickpublic@gmail.com>
2024-10-01 00:27:08 +0200

    fix fast and fecious, also sim0 will fix the rest

--- achievements/poop/fastAndFecious.ts
+++ achievements/poop/fastAndFecious.ts
@@ -1,16 +1,19 @@
 import { poopLeaderboardWithFilter, addAchievementToUser, getAchievement } from '../../database'
-import moment from 'moment'
+import moment from 'moment-timezone'
 import { Poop } from '../../types/Poop'
 import { RawUser } from '../../types/User'
 import { Message } from 'whatsapp-web.js'
 import { Achievement } from '../../types/Achievement'
+import config from '../../config.json'
+
+const timezone = config.timezone || 'UTC'
 
 const fastAndFecious: Achievement = {
 	id: 'FAST_AND_FECIOUS',
 	check: function (poop: Poop, user: RawUser, message: Message) {
-		const year = moment().year()
-		const month = moment().month()
-		const leaderboard = poopLeaderboardWithFilter(year, month)
+		const year = moment().tz(timezone).year()
+		const month = moment().tz(timezone).month()
+		const leaderboard = poopLeaderboardWithFilter(year, month + 1)
 		if (leaderboard.length == 1) {
 			addAchievementToUser(user.id, this.id)
 			const achievement = getAchievement(this.id)
