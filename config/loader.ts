03d409432eaea612d79bb68b3263bfe007903907
iQuick <iquickpublic@gmail.com>
2024-12-22 11:52:29 +0100

    allow the webserver to start without a valid config

--- config/loader.ts
+++ config/loader.ts
@@ -2,6 +2,8 @@ import fs from 'fs'
 import path from 'path'
 import { Config } from '../types/Config'
 
+export let isLoaded = false
+
 export let config: Config = {
 	name: '',
 	description: '',
@@ -14,28 +16,41 @@ export let config: Config = {
 }
 
 export function loadConfig() {
-	switch (process.env.ENVIRONMENT) {
-		case 'dev':
-			config = JSON.parse(
-				fs.readFileSync(path.join('./config', 'config_dev.json'), 'utf-8'),
-			)
-			break
-		case 'production':
-			config = JSON.parse(
-				fs.readFileSync(path.join('./config', 'config_prod.json'), 'utf-8'),
-			)
-			break
-		default:
-			console.error()
-			console.error('███████ ██████  ██████   ██████  ██████  ')
-			console.error('██      ██   ██ ██   ██ ██    ██ ██   ██ ')
-			console.error('█████   ██████  ██████  ██    ██ ██████  ')
-			console.error('██      ██   ██ ██   ██ ██    ██ ██   ██ ')
-			console.error('███████ ██   ██ ██   ██  ██████  ██   ██ ')
-			console.error()
-			console.error(
-				'[ENVIRONMENT] The application cannot start because no valid environment has been specified',
-			)
-			process.exit(1)
+	try {
+		switch (process.env.ENVIRONMENT) {
+			case 'dev':
+				config = JSON.parse(
+					fs.readFileSync(path.join('./config', 'config_dev.json'), 'utf-8'),
+				)
+				isLoaded = true
+				break
+			case 'production':
+				config = JSON.parse(
+					fs.readFileSync(path.join('./config', 'config_prod.json'), 'utf-8'),
+				)
+				isLoaded = true
+				break
+			default:
+				printErrorBanner()
+				console.error(
+					'[ENVIRONMENT] The application cannot start because no valid environment has been specified',
+				)
+				process.exit(1)
+		}
+	} catch (e) {
+		printErrorBanner()
+		console.error(
+			'[CONFIG] The application cannot start because the configuration file was not found',
+		)
 	}
+}
+
+function printErrorBanner() {
+	console.error()
+	console.error('███████ ██████  ██████   ██████  ██████  ')
+	console.error('██      ██   ██ ██   ██ ██    ██ ██   ██ ')
+	console.error('█████   ██████  ██████  ██    ██ ██████  ')
+	console.error('██      ██   ██ ██   ██ ██    ██ ██   ██ ')
+	console.error('███████ ██   ██ ██   ██  ██████  ██   ██ ')
+	console.error()
 }
--- index.ts
+++ index.ts
@@ -12,176 +12,215 @@ import fastifyStatic from '@fastify/static'
 import { version } from './package.json'
 //@ts-ignore
 import schedule from 'node-schedule'
-import { config, loadConfig } from './config/loader'
+import { isLoaded, config, loadConfig } from './config/loader'
 dotenv.config()
+
 loadConfig()
 
-console.log(`
-▄████▄   ▄▄▄       ▄████▄   ▄████▄   ▄▄▄       ▄▄▄▄    ▒█████  ▄▄▄█████▓
-▒██▀ ▀█  ▒████▄    ▒██▀ ▀█  ▒██▀ ▀█  ▒████▄    ▓█████▄ ▒██▒  ██▒▓  ██▒ ▓▒
-▒▓█    ▄ ▒██  ▀█▄  ▒▓█    ▄ ▒▓█    ▄ ▒██  ▀█▄  ▒██▒ ▄██▒██░  ██▒▒ ▓██░ ▒░
-▒▓▓▄ ▄██▒░██▄▄▄▄██ ▒▓▓▄ ▄██▒▒▓▓▄ ▄██▒░██▄▄▄▄██ ▒██░█▀  ▒██   ██░░ ▓██▓ ░ 
-▒ ▓███▀ ░ ▓█   ▓██▒▒ ▓███▀ ░▒ ▓███▀ ░ ▓█   ▓██▒░▓█  ▀█▓░ ████▓▒░  ▒██▒ ░ 
-░ ░▒ ▒  ░ ▒▒   ▓▒█░░ ░▒ ▒  ░░ ░▒ ▒  ░ ▒▒   ▓▒█░░▒▓███▀▒░ ▒░▒░▒░   ▒ ░░   
-  ░  ▒     ▒   ▒▒ ░  ░  ▒     ░  ▒     ▒   ▒▒ ░▒░▒   ░   ░ ▒ ▒░     ░    
-░          ░   ▒   ░        ░          ░   ▒    ░    ░ ░ ░ ░ ▒    ░      
-░ ░            ░  ░░ ░      ░ ░            ░  ░ ░          ░ ░           
-░                  ░        ░                        ░                    
-`)
+if (!isLoaded)
+{
+	server.get('/', (request: FastifyRequest, reply: FastifyReply) =>
+	{
+		reply.send("The application cannot start because the configuration file was not found")
+	})
 
-console.log(
-	`Loaded the following configuration for environment ${process.env.ENVIRONMENT}`,
-)
-console.log(config)
+	server.listen(
+		{ host: '0.0.0.0', port: process.env.SERVER_PORT ?? 3000 }
+	)
 
-server.register(require('@fastify/swagger'), {
-	swagger: {
-		info: {
-			title: 'CaccaBOT API',
-			description: 'CaccaBOT API Documentation',
-			version,
-		},
-		host: `caccabot.duckdns.org`,
-		schemes: ['https'],
-		consumes: ['application/json'],
-		produces: ['application/json'],
-	},
-})
-
-server.register(require('@fastify/swagger-ui'), {
-	routePrefix: '/docs',
-	uiConfig: {
-		deepLinking: false,
-		syntaxHighlight: {
-			activate: true,
-			theme: 'nord',
-		},
-	},
-	uiHooks: {
-		onRequest: function (
-			request: FastifyRequest,
-			reply: FastifyReply,
-			next: () => void,
-		) {
-			next()
-		},
-		preHandler: function (
-			request: FastifyRequest,
-			reply: FastifyReply,
-			next: () => void,
-		) {
-			next()
-		},
-	},
-})
-
-server.register(import('@fastify/autoload'), {
-	dir: `${__dirname}/api`,
-	dirNameRoutePrefix: true,
-	options: { prefix: '/api' },
-})
-
-server.addHook('onRoute', (routeOptions: { url: string; method: string }) => {
-	if (routeOptions.url.startsWith('/api') && routeOptions.method != 'HEAD') {
-		console.log(`[ENDPOINT] ${routeOptions.method} ${routeOptions.url}`)
-	}
-})
-
-server.register(import('@fastify/compress'))
-
-server.register(fastifyStatic, {
-	root: path.join(__dirname, 'public'),
-	prefix: '/public',
-})
-
-server.register(fastifyStatic, {
-	root: path.join(__dirname, '/public/client'),
-	prefix: '/',
-	decorateReply: false,
-})
-
-server.register(fastifyStatic, {
-	root: path.join(__dirname, '/public/client/assets'),
-	prefix: '/assets/',
-	decorateReply: false,
-})
-
-server.register(fastifyStatic, {
-	root: path.join(__dirname, '/public/collectibles'),
-	prefix: '/collectibles/',
-	decorateReply: false,
-	maxAge: '1d',
-	immutable: true,
-})
-
-server.register(fastifyStatic, {
-	root: path.join(__dirname, '/public/pfp'),
-	prefix: '/pfp/',
-	decorateReply: false,
-})
-
-server.register(import('@fastify/cors'), {
-	origin: '*',
-})
-
-server.addHook(
-	'onRequest',
-	(req: { method: any; url: any }, res: any, done: () => void) => {
-		const log = `${new Date().toISOString()} | ${req.method} | ${req.url}`
-		if (!fs.existsSync(`${__dirname}/logs`)) {
-			fs.mkdirSync(`${__dirname}/logs`)
-		}
-		fs.appendFileSync(
-			`${__dirname}/logs/${new Date().toISOString().slice(0, 10)}.log`,
-			`${log}\n`,
-		)
-		console.info(log)
-		done()
-	},
-)
-
-server.decorate('NotFound', (req: FastifyRequest, res: FastifyReply) => {
-	if (req.url.toLowerCase().startsWith('/api')) {
-		res.code(404).send({ error: 'This endpoint does not exist' })
-	}
-
-	const stream = fs.createReadStream(`${__dirname}/public/client/index.html`)
-	res.type('text/html').send(stream)
-})
-
-server.setNotFoundHandler(server.NotFound)
-
-if (config.whatsappModuleEnabled) {
-	client.initialize()
 }
 
-async function initJobs() {
-	const jobsDir = fs
-		.readdirSync(`${path.resolve('./jobs')}`)
-		.filter((file) => file.endsWith('.ts'))
+if (isLoaded)
+{
+	console.log(`
+		▄████▄   ▄▄▄       ▄████▄   ▄████▄   ▄▄▄       ▄▄▄▄    ▒█████  ▄▄▄█████▓
+		▒██▀ ▀█  ▒████▄    ▒██▀ ▀█  ▒██▀ ▀█  ▒████▄    ▓█████▄ ▒██▒  ██▒▓  ██▒ ▓▒
+		▒▓█    ▄ ▒██  ▀█▄  ▒▓█    ▄ ▒▓█    ▄ ▒██  ▀█▄  ▒██▒ ▄██▒██░  ██▒▒ ▓██░ ▒░
+		▒▓▓▄ ▄██▒░██▄▄▄▄██ ▒▓▓▄ ▄██▒▒▓▓▄ ▄██▒░██▄▄▄▄██ ▒██░█▀  ▒██   ██░░ ▓██▓ ░ 
+		▒ ▓███▀ ░ ▓█   ▓██▒▒ ▓███▀ ░▒ ▓███▀ ░ ▓█   ▓██▒░▓█  ▀█▓░ ████▓▒░  ▒██▒ ░ 
+		░ ░▒ ▒  ░ ▒▒   ▓▒█░░ ░▒ ▒  ░░ ░▒ ▒  ░ ▒▒   ▓▒█░░▒▓███▀▒░ ▒░▒░▒░   ▒ ░░   
+		  ░  ▒     ▒   ▒▒ ░  ░  ▒     ░  ▒     ▒   ▒▒ ░▒░▒   ░   ░ ▒ ▒░     ░    
+		░          ░   ▒   ░        ░          ░   ▒    ░    ░ ░ ░ ░ ▒    ░      
+		░ ░            ░  ░░ ░      ░ ░            ░  ░ ░          ░ ░           
+		░                  ░        ░                        ░                    
+		`)
 
-	for (const jobFile of jobsDir) {
-		const job = await import(`${path.resolve('./jobs')}/${jobFile}`)
-		schedule.scheduleJob(job.default.interval, job.default.execute)
-		console.info(`[JOB] ${job.default.interval} => ${job.default.name}`)
-	}
-}
 
-server.listen(
-	{ host: '0.0.0.0', port: process.env.SERVER_PORT ?? 3000 },
-	async (err: any, address: string) => {
-		if (err) {
-			console.error(err)
-			process.exit(1)
-		}
-		initDatabase()
-		await initJobs()
-		if (config.monthlyPurge) {
-			console.warn(
-				'[WARNING] Monthly Purge is enabled, users who have been ' +
-					'inactive for more than a month will be deleted at month reset!',
+	console.log(
+		`Loaded the following configuration for environment ${process.env.ENVIRONMENT}`,
+	)
+	console.log(config)
+
+	server.register(require('@fastify/swagger'), {
+		swagger: {
+			info: {
+				title: 'CaccaBOT API',
+				description: 'CaccaBOT API Documentation',
+				version,
+			},
+			host: `caccabot.duckdns.org`,
+			schemes: ['https'],
+			consumes: ['application/json'],
+			produces: ['application/json'],
+		},
+	})
+
+	server.register(require('@fastify/swagger-ui'), {
+		routePrefix: '/docs',
+		uiConfig: {
+			deepLinking: false,
+			syntaxHighlight: {
+				activate: true,
+				theme: 'nord',
+			},
+		},
+		uiHooks: {
+			onRequest: function (
+				request: FastifyRequest,
+				reply: FastifyReply,
+				next: () => void,
 			)
+			{
+				next()
+			},
+			preHandler: function (
+				request: FastifyRequest,
+				reply: FastifyReply,
+				next: () => void,
+			)
+			{
+				next()
+			},
+		},
+	})
+
+	server.register(import('@fastify/autoload'), {
+		dir: `${__dirname}/api`,
+		dirNameRoutePrefix: true,
+		options: { prefix: '/api' },
+	})
+
+	server.addHook('onRoute', (routeOptions: { url: string; method: string }) =>
+	{
+		if (routeOptions.url.startsWith('/api') && routeOptions.method != 'HEAD')
+		{
+			console.log(`[ENDPOINT] ${routeOptions.method} ${routeOptions.url}`)
 		}
-		console.log('[WEBSERVER] Ready on ' + address)
-	},
-)
+	})
+
+	server.register(import('@fastify/compress'))
+
+	server.register(fastifyStatic, {
+		root: path.join(__dirname, 'public'),
+		prefix: '/public',
+	})
+
+	server.register(fastifyStatic, {
+		root: path.join(__dirname, '/public/client'),
+		prefix: '/',
+		decorateReply: false,
+	})
+
+	server.register(fastifyStatic, {
+		root: path.join(__dirname, '/public/client/assets'),
+		prefix: '/assets/',
+		decorateReply: false,
+	})
+
+	server.register(fastifyStatic, {
+		root: path.join(__dirname, '/public/collectibles'),
+		prefix: '/collectibles/',
+		decorateReply: false,
+		maxAge: '1d',
+		immutable: true,
+	})
+
+	server.register(fastifyStatic, {
+		root: path.join(__dirname, '/public/pfp'),
+		prefix: '/pfp/',
+		decorateReply: false,
+	})
+
+	server.register(import('@fastify/cors'), {
+		origin: '*',
+	})
+
+	server.addHook(
+		'onRequest',
+		(req: { method: any; url: any }, res: any, done: () => void) =>
+		{
+			const log = `${new Date().toISOString()} | ${req.method} | ${req.url}`
+			if (!fs.existsSync(`${__dirname}/logs`))
+			{
+				fs.mkdirSync(`${__dirname}/logs`)
+			}
+			fs.appendFileSync(
+				`${__dirname}/logs/${new Date().toISOString().slice(0, 10)}.log`,
+				`${log}\n`,
+			)
+			console.info(log)
+			done()
+		},
+	)
+
+	server.decorate('NotFound', (req: FastifyRequest, res: FastifyReply) =>
+	{
+		if (req.url.toLowerCase().startsWith('/api'))
+		{
+			res.code(404).send({ error: 'This endpoint does not exist' })
+		}
+
+		const stream = fs.createReadStream(`${__dirname}/public/client/index.html`)
+		res.type('text/html').send(stream)
+	})
+
+	server.setNotFoundHandler(server.NotFound)
+
+	if (config.whatsappModuleEnabled)
+	{
+		const sessionLockFile = path.join('./.wwebjs_auth', 'session', 'SingletonLock')
+		if (fs.existsSync(sessionLockFile))
+		{
+			fs.rmSync(sessionLockFile)
+		}
+
+		client.initialize()
+	}
+
+	async function initJobs()
+	{
+		const jobsDir = fs
+			.readdirSync(`${path.resolve('./jobs')}`)
+			.filter((file) => file.endsWith('.ts'))
+
+		for (const jobFile of jobsDir)
+		{
+			const job = await import(`${path.resolve('./jobs')}/${jobFile}`)
+			schedule.scheduleJob(job.default.interval, job.default.execute)
+			console.info(`[JOB] ${job.default.interval} => ${job.default.name}`)
+		}
+	}
+
+	server.listen(
+		{ host: '0.0.0.0', port: process.env.SERVER_PORT ?? 3000 },
+		async (err: any, address: string) =>
+		{
+			if (err)
+			{
+				console.error(err)
+				process.exit(1)
+			}
+			initDatabase()
+			await initJobs()
+			if (config.monthlyPurge)
+			{
+				console.warn(
+					'[WARNING] Monthly Purge is enabled, users who have been ' +
+					'inactive for more than a month will be deleted at month reset!',
+				)
+			}
+			console.log('[WEBSERVER] Ready on ' + address)
+		},
+	)
+
+}
