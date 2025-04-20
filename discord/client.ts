89d5299306a9afa28aca11f6adb876b1f76d31f7
iQuick <iquickpublic@gmail.com>
2025-04-21 00:53:17 +0200

    add deploy discord commands script + uptime command

--- discord/client.ts
+++ discord/client.ts
@@ -1,14 +1,36 @@
 import { Client, Events, GatewayIntentBits } from 'discord.js'
 import log from 'loglevel'
-
-const token = process.env.DISCORD_TOKEN
+import fs from 'fs'
+import path from 'path'
 
 export const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessages] })
+export let commands = new Map();
 
-client.once(Events.ClientReady, readyClient => {
-	log.info(`[DISCORD] Ready on ${readyClient.user.tag}`);
+const commandFiles = fs.readdirSync(path.join(__dirname, 'commands')).filter(file => file.endsWith('.js') || file.endsWith('.ts'));
+
+client.once(Events.ClientReady, async readyClient => {
+
+    for (const file of commandFiles) {
+        const imported = await import(`./commands/${file}`)
+        const command = imported.default;
+        commands.set(command.data.name, command);
+    }
+
+    log.info(`[DISCORD] Ready on ${readyClient.user.tag}`)
 })
 
-client.on(Events.MessageCreate, message => {
-    console.log(`[DISCORD] Message from ${message.author.tag}: ${message.content}`);
-})
+client.on('interactionCreate', async (interaction) => {
+    if (!interaction.isCommand()) return;
+
+    const command = commands.get(interaction.commandName)
+
+    if (command) {
+        try {
+
+            await command.execute(interaction);
+        } catch (error) {
+            console.error('Error executing command:', error);
+            await interaction.reply('There was an error while executing this command!');
+        }
+    }
+});
