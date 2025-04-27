540aa264bfce64828e7514281e645b44d207f51c
iQuick <iquickpublic@gmail.com>
2025-04-27 23:22:23 +0200

    fixed discord linking dialog layout

--- client/src/views/DiscordLink.vue
+++ client/src/views/DiscordLink.vue
@@ -7,7 +7,6 @@ import { useAPIStore } from "../stores/api"
 import TablerArrowsDiff from '~icons/tabler/arrows-diff'
 import IcBaselineDiscord from '~icons/ic/baseline-discord'
 import HeroiconsCheck20Solid from '~icons/heroicons/check-20-solid'
-import HeroiconsXMark from '~icons/heroicons/x-mark'
 import HeroiconsOutlineRefresh from '~icons/heroicons-outline/refresh'
 import router from "../router/router"
 import { useToast } from "vue-toastification"
@@ -55,8 +54,7 @@ onMounted(async () => {
 </script>
 
 <template>
-    <div v-if="loading"
-        class="loader-wrapper flex h-[85vh] w-full items-center justify-center">
+    <div v-if="loading" class="loader-wrapper flex h-[85vh] w-full items-center justify-center">
         <span class="loading loading-spinner loading-lg"></span>
     </div>
     <div v-if="!loading" class="discord-link-wrapper w-full h-[85vh] flex items-center justify-center">
@@ -70,20 +68,20 @@ onMounted(async () => {
                 Connect your CaccaBOT account to Discord in just one click and unlock all features!
             </p>
 
-            <div class="flex flex-row justify-center items-center space-x-10 mb-8">
+            <div class="flex flex-row justify-center items-center space-x-5 md:space-x-10 mb-8">
                 <div
-                    class="ring-4 ring-primary ring-offset-2 ring-offset-base-100 rounded-full p-5 hover:scale-110 transition-transform">
-                    <img :src="Asset.HOME" class="w-20 h-20" />
+                    class="flex justify-center items-center ring-4 ring-primary w-16 h-16 md:w-32 md:h-32 ring-offset-2 ring-offset-base-100 rounded-full p-3 md:p-5 hover:scale-110 transition-transform">
+                    <img :src="Asset.HOME" class="h-10 w-10 md:w-20 md:h-20" />
                 </div>
                 <TablerArrowsDiff class="text-5xl text-secondary animate-pulse" />
                 <div
-                    class="ring-4 ring-primary ring-offset-2 ring-offset-base-100 rounded-full p-5 hover:scale-110 transition-transform">
-                    <IcBaselineDiscord class="text-7xl text-primary" />
+                    class="flex justify-center items-center ring-4 ring-primary w-16 h-16 md:w-32 md:h-32 ring-offset-2 ring-offset-base-100 rounded-full p-3 md:p-5 hover:scale-110 transition-transform">
+                    <IcBaselineDiscord class="text-4xl md:text-7xl text-primary" />
                 </div>
             </div>
 
             <button @click="link()"
-                class="btn btn-primary mt-5 text-xl px-10 py-4 shadow-md hover:scale-105 transition-transform">
+                class="btn btn-primary mt-5 text-lg md:text-xl px-10 py-4 shadow-md hover:scale-105 transition-transform">
                 <TablerArrowsDiff class="mr-2" /> Link account
             </button>
         </div>
@@ -118,7 +116,7 @@ onMounted(async () => {
             <h1 class="font-bold text-4xl mb-3 text-center text-primary">
                 Link CaccaBOT with Discord
             </h1>
-            <p class="text-base text-base-content/70 mb-8 text-center max-w-md">
+            <p class="text-base text-base-content/70 my-4 text-center max-w-md">
                 An error has occurred while linking your account. Please try again later.
             </p>
 
